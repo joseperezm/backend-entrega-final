306cd8d0d13675da3c6831e8acdae139073b4375
joseperezm <coderhouse@jjpm.cl>
2024-06-27 01:29:41 -0400

    Crear una vista para visualizar, modificar el rol y eliminar un usuario.

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -5,9 +5,9 @@ const redirectIfNotLoggedInApi = require('../middleware/auth.js');
 const authorizeApi = require('../middleware/authorizeApi.js');
 const upload = require('../middleware/multer');
 
-router.delete('/', authorizeApi('admin'), userController.deleteInactiveUsers);
+router.delete('/', authorizeApi('admin'), redirectIfNotLoggedInApi, userController.deleteInactiveUsers);
 router.delete('/:uid', authorizeApi('admin'), redirectIfNotLoggedInApi, userController.deleteUser);
-router.get('/', authorizeApi('admin'), userController.getAllUsers);
+router.get('/', authorizeApi('admin'), redirectIfNotLoggedInApi, userController.getAllUsers);
 router.put('/premium/:uid', authorizeApi('admin'), redirectIfNotLoggedInApi, userController.changeUserRole);
 router.post('/:uid/documents', upload.array('documents'), redirectIfNotLoggedInApi, userController.uploadDocuments);
 
